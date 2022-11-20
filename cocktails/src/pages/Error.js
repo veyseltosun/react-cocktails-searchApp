bbc34b45c0488899add88889cd2cd825cd7c3503
veyseltosun <postalarim1923@gmail.com>
2022-11-20 17:20:04 +0300

    error .

--- cocktails/src/pages/Error.js
+++ cocktails/src/pages/Error.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
+
 
 function Error() {
   return (
-    <div>Error</div>
+    <section className='error-page section'>
+      <div className=" error-container ">
+        <h1>oops! it's a dead end</h1>
+        <Link to="/" className="btn btn-primary">
+          back home
+        </Link>
+      </div>
+      
+    </section>
   )
 }
 
