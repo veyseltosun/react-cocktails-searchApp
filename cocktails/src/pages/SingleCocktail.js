c103e02a8673464c97a90e96d2755a5176bc58c5
veyseltosun <postalarim1923@gmail.com>
2022-11-21 17:09:33 +0300

    getting start new week.

--- cocktails/src/pages/SingleCocktail.js
+++ cocktails/src/pages/SingleCocktail.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Form } from 'react-router-dom';
+import Loading from '../components/Loading';
+import {useParams, Link} from "react-router-dom";
+const url = "`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=`"
 
 function SingleCocktail() {
+
+  const [loading, setloading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() =>{
+    setloading(true);
+    async function getCoctail(){
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
+  })
   return (
     <div>SingleCocktail</div>
   )
